d518c12181a8b0ccf3a56b4a0bb6b873ccd0b6db
Michael Ziliani <zililanimichael7@gmail.com>
2025-07-30 13:27:32 -0500

    add tokenJWT

--- src/main/java/foro/hub/api/controller/AutenticacionController.java
+++ src/main/java/foro/hub/api/controller/AutenticacionController.java
@@ -1,26 +1,36 @@
 package foro.hub.api.controller;
 
+import foro.hub.api.domain.usuario.DatosAutenticacion;
+import foro.hub.api.domain.usuario.Usuario;
+import foro.hub.api.infra.security.TokenService;
+import foro.hub.api.infra.security.datosTokenJWT;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import foro.hub.api.domain.usuario.DatosAutenticacion;
 
 @RestController
 @RequestMapping("/login")
 public class AutenticacionController {
 
     @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private AuthenticationManager manager;
 
-    public ResponseEntity iniciarSecion(@RequestBody @Valid DatosAutenticacion datos) {
-        var token = new UsernamePasswordAuthenticationToken(datos.correo(), datos.contrasena());
-        var  autenticacion = manager.authenticate(token);
+ @PostMapping
+ public ResponseEntity iniciarSecion (@RequestBody @Valid DatosAutenticacion datos) {
+     var atenticationToken = new UsernamePasswordAuthenticationToken(datos.correo(),datos.contrasena());
+     var autenticacion = manager.authenticate(atenticationToken);
 
-        return ResponseEntity.ok().build();
-    }
+     var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+
+     return ResponseEntity.ok(new datosTokenJWT(tokenJWT));
+ }
 }
--- src/main/java/foro/hub/api/domain/usuario/Usuario.java
+++ src/main/java/foro/hub/api/domain/usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -14,7 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,5 +57,40 @@ public class Usuario {
 
     public void activarUsuario() {
         this.activo = true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return correo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
